24de1660278cb7ed8fff96a3b7e4a4c659315c8b
Linus Elvius <135825781+linx02@users.noreply.github.com>
2025-12-01 18:47:13 +0100

    2025 day 1 part 2

--- 2025/day_1/solve.py
+++ 2025/day_1/solve.py
@@ -2,30 +2,47 @@ with open("input.txt", "r") as f:
     lines = [l.strip() for l in f.readlines()]
 
 def rotate(i, d, v):
-    while(v != 0):
+    c1 = 0
+    c2 = 0
+    while v != 0:
         if d == 'L':
-            if i - v < 0:
-                v -= i + 1
-                i = 99
-            else:
-                i -= v
+            i -= 1
+            v -= 1
+            if i == -1: i = 99
+            if v == 0 and i == 0:
+                c1 += 1
                 break
-        elif d == 'R':
-            if i + v > 99:
-                v -= 99 - i + 1
-                i = 0
-            else:
-                i += v
+            if v != 0 and i == 0: c2 += 1
+        if d == 'R':
+            i += 1
+            v -= 1
+            if i == 100: i = 0
+            if v == 0 and i == 0:
+                c1 += 1
                 break
-    return i
+            if v != 0 and i == 0: c2 += 1
+    return i, c1, c2
 
-i = 50
-c = 0
-for l in lines:
-    d = l[0]
-    v = int(l[1:])
-    i = rotate(i, d, v)
-    if i == 0:
-        c += 1
 
-print(c)
+def solve1():
+    i = 50
+    c = 0
+    for l in lines:
+        d = l[0]
+        v = int(l[1:])
+        i, c1, c2 = rotate(i, d, v)
+        c += c1
+    return c
+
+def solve2():
+    i = 50
+    c = 0
+    for l in lines:
+        d = l[0]
+        v = int(l[1:])
+        i, c1, c2 = rotate(i, d, v)
+        c += c1 + c2
+    return c
+
+print(solve1())
+print(solve2())
