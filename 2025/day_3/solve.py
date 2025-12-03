064523c87ef2f5227223b6b87bd6441bbc9dd929
Linus Elvius <135825781+linx02@users.noreply.github.com>
2025-12-03 17:14:44 +0100

    2025 Day 3 part 2

--- 2025/day_3/solve.py
+++ 2025/day_3/solve.py
@@ -1,17 +1,28 @@
 with open("input.txt", "r") as f:
     banks = [l.strip() for l in f.readlines()]
 
-r = 0
-for b in banks:
-    b = b[::-1]
-    cmp1 = 1
-    for pos, b_ in enumerate(b[2:]):
-        if b_ >= b[cmp1]: cmp1 = pos + 2
-    cmp2 = 0
-    for pos, b_ in enumerate(b[1:]):
-        if pos + 1 == cmp1: break
-        if b_ >= b[cmp2]: cmp2 = pos + 1
-    b = b[::-1]
-    r += int(str(b[len(b) - cmp1 - 1] + b[len(b) - cmp2 - 1]))
+def scan(banks, n):
+    r = []
+    for b in banks:
+        sp = 0
+        bp = len(b) - n + 1
+        pos_arr = []
+        for i in range(n):
+            m = sp
+            for j in range(sp, bp):
+                if b[j] > b[m]: m = j
+            pos_arr.append(m)
+            bp += 1
+            sp = m + 1
+        r.append(''.join([str(b[v]) for v in pos_arr]))
+    return sum([int(v) for v in r])
 
-print(r)
+def solve1():
+    return scan(banks, 2)
+
+def solve2():
+    return scan(banks, 12)
+
+
+print(solve1())
+print(solve2())
