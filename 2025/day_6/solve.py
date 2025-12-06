58453d72579ff3d812b512e2f107deab7b50e6cb
Linus Elvius <135825781+linx02@users.noreply.github.com>
2025-12-06 14:02:00 +0100

    2025 Day 6 part 2

--- 2025/day_6/solve.py
+++ 2025/day_6/solve.py
@@ -1,17 +1,57 @@
 with open('input.txt') as f:
-    lines = [l.strip() for l in f.readlines()]
+    lines = f.readlines()
 
-problems = [[] for n in lines[0].split()]
+def solve1():
+    global lines
+    a = 0
+    lines_ = [l.strip() for l in lines]
+    problems = [[] for n in lines_[0].split()]
 
-for i in range(len(lines[0].split())):
-    for j in range(len(lines)):
-        problems[i].append(lines[j].split()[i])
+    for i in range(len(lines_[0].split())):
+        for j in range(len(lines_)):
+            problems[i].append(lines_[j].split()[i])
 
-a = 0
-for p in problems:
-    o = p[-1]
-    p = p[:-1]
-    e = o.join(p)
-    a += eval(e)
+    for p in problems:
+        o = p[-1]
+        p = p[:-1]
+        e = o.join(p)
+        a += eval(e)
+    return a
 
-print(a)
+def solve2():
+    global lines
+    a = 0
+    problems = [[] for i in range(len(lines[0].split()))]
+    cols = []
+
+    for i in range(len(lines[0]) - 1):
+        col = []
+        for j in range(len(lines)):
+            col.append(lines[j][i])
+        cols.append(col)
+
+    cols = cols[::-1]
+
+    i = 0
+    eq = []
+    for j, col in enumerate(cols):
+        c = ''.join(col).strip()
+        if c == '': continue
+        if c[-1] in ['+', '*', '/', '-']:
+            o = c[-1]
+            c = c[:-1]
+            eq.append(c)
+            problems[i] = o.join(eq)
+            problems[i] = problems[i].strip()
+            eq = []
+            i += 1
+            continue
+        eq.append(c)
+
+    for p in problems:
+        a += eval(p)
+    return a
+    
+
+print(solve1())
+print(solve2())
